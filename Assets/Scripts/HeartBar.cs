73f909bb6ed6b123b867200e1385cba1364ab500
JDGuevara7 <144757809+JDGuevara7@users.noreply.github.com>
2024-10-14 02:03:25 -0400

    Added functionality to health bar - Juan
    Still needs to be fixed

--- Assets/Scripts/HeartBar.cs
+++ Assets/Scripts/HeartBar.cs
@@ -5,9 +5,21 @@ using UnityEngine;
 public class HeartBar : MonoBehaviour
 {
     public GameObject heartPrefab;
-    public float health, maxHealth;
+    public Player player;
 
     List<HealthHeart> hearts = new List<HealthHeart>();
+
+    private void OnEnable() {
+        player.OnPlayerDamaged += drawHearts;
+    }
+
+    private void OnDisable() {
+        player.OnPlayerDamaged -= drawHearts;
+    }
+
+    private void Start() {
+        drawHearts();
+    }
 
     public void ClearHearts() {
         foreach (Transform t in transform) {
@@ -25,8 +37,17 @@ public class HeartBar : MonoBehaviour
         hearts.Add(heartComponent);
     }
 
-    // public void drawHearts() {
-    //     ClearHearts();
-    //     float maxHealthRemainder = player
-    // }
+    public void drawHearts() {
+        ClearHearts();
+        float maxHealthRemainder = player.maxHealth % 2;
+        int heartsToMake = (int)((player.maxHealth / 20) + maxHealthRemainder);
+        for(int i = 0; i < heartsToMake; i++) {
+            createEmptyHeart();
+        }
+
+        for (int i = 0; i < hearts.Count; i++) {
+            int HeartStatusRemainder = (int)Mathf.Clamp(player.health - (i * 10), 0, 1);
+            hearts[i].setHeartImage((HeartStatus)HeartStatusRemainder);
+        }
+    }
 }
